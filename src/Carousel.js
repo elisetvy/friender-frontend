0369e608d4709a9deac4da9b44798551662cda52
Elise Ngo <elisetvy@gmail.com>
2023-11-08 08:24:31 -0800

    Style swipe/unswipe buttons

--- src/Carousel.js
+++ src/Carousel.js
@@ -18,14 +18,10 @@ function Carousel({ users, currUser }) {
   }
 
   return (
-    <div className="flex flex-col items-center gap-3">
-      <div className="flex justify-center w-full">
-        <User user={currU} currUser={currUser}/>
-      </div>
-      <div className="flex gap-2">
-        <button className="bg-emerald-100 px-3 py-1 rounded-lg hover:text-black hover:scale-105" onClick={unswipe}>Unswipe</button>
-        <button className="bg-emerald-100 px-3 py-1 rounded-lg hover:text-black hover:scale-105" onClick={swipe}>Swipe</button>
-      </div>
+    <div className="flex justify-center items-center gap-3">
+      <button className={`${index === 0 ? "invisible" : "visible"} h-fit bg-emerald-300 px-3 py-1 rounded-lg hover:text-black hover:scale-105`} onClick={unswipe}>Unswipe</button>
+        <User className="z-0" user={currU} currUser={currUser}/>
+      <button className={`${index === users.length - 1 ? "invisible" : "visible"} h-fit bg-emerald-300 px-3 py-1 rounded-lg hover:text-black hover:scale-105`} onClick={swipe}>Swipe</button>
     </div>
   )
 
