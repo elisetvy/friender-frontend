8605dfebbb8b95a13f7b1dfb043aceedcc02c982
Elise Ngo <elisetvy@gmail.com>
2023-11-13 23:18:01 -0800

    Finish input validation/error handling

--- src/RegisterForm.js
+++ src/RegisterForm.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import calculateAge from "./utils";
 
@@ -19,19 +19,42 @@ function RegisterForm({ register }){
                           bio: "",
                           }
 
-  const [form, setForm] = useState(1);
+  const [form, setForm] = useState("one");
   const [formData, setFormData]= useState(initialFormData);
   const [error, setError] = useState(null);
+  const [emails, setEmails] = useState(null);
 
-  async function checkUsername(e) {
+  useEffect(function getUsersOnMount() {
+    async function getUsers() {
+      const users = await FrienderApi.getUsers();
+      const emails = users.map(user => user.email);
+      setEmails(emails);
+    }
+    getUsers();
+  }, []);
+
+  async function checkOne(e) {
     e.preventDefault();
 
     try {
       await FrienderApi.getUser(formData.username);
-      setError(`User already exists: ${formData.username}`)
+      setError(`User already exists: ${formData.username}`);
     } catch(err) {
-      console.log(err)
-      setForm(2);
+      setError(null);
+      setForm("two");
+    }
+  }
+
+  async function checkTwo(e) {
+    e.preventDefault();
+
+    if (calculateAge(formData.dob) < 18) {
+      setError("You must be at least 18 years old to register!");
+    } else if (emails.includes(formData.email)) {
+      setError("This email address is already associated with a user!");
+    } else {
+      setError(null);
+      setForm("three");
     }
   }
 
@@ -59,11 +82,11 @@ function RegisterForm({ register }){
   return(
     <div className="w-full h-full flex justify-center items-center">
       <div className="bg-emerald-100 w-2/5 flex flex-col justify-center items-center text-center px-14 py-10 rounded-lg">
-        { form === 1 &&
+        { form === "one" &&
         <>
         <p className="text-lg font-bold">Sign up for Friender today</p>
         <p className="text-sm">Choose a username and password to begin making friends!</p>
-        <form onSubmit={checkUsername} className="mt-4 flex flex-col text-left w-full">
+        <form onSubmit={checkOne} className="mt-4 flex flex-col text-left w-full">
           <label className="mb-1">Username</label>
           <input name="username"
               required
@@ -97,9 +120,9 @@ function RegisterForm({ register }){
         </div>
         </>
       }
-      { form === 2 &&
+      { form === "two" &&
       <>
-      <form onSubmit={(e) => { e.preventDefault(); setForm(3); }} className="mt-4 flex flex-col text-left w-full">
+      <form onSubmit={checkTwo} className="mt-4 flex flex-col text-left w-full">
         <label className="mb-1">Name</label>
         <input name="name"
               required
@@ -126,21 +149,22 @@ function RegisterForm({ register }){
           type="date"
           className="mb-3 rounded-lg px-2 py-1"
           />
+        {error && <p className="mb-3 text-red-400 font-bold text-center">{error}</p>}
         <div className="flex justify-end">
         <button className="bg-emerald-300 w-fit px-3 py-1 text-sm rounded-lg hover:scale-105">Continue <i className="bi bi-arrow-right"></i></button>
         </div>
       </form>
       <div className="mt-4 flex gap-2">
-        <i onClick={(e) => { e.preventDefault(); setForm(1); }} className="hover:cursor-pointer bi bi-circle-fill text-white text-[8px]"></i>
+        <i onClick={(e) => { e.preventDefault(); setForm("one"); setError(null); }} className="hover:cursor-pointer bi bi-circle-fill text-white text-[8px]"></i>
         <i className="bi bi-circle-fill text-emerald-300 text-[8px]"></i>
         <i className="bi bi-circle-fill text-white text-[8px]"></i>
         <i className="bi bi-circle-fill text-white text-[8px]"></i>
       </div>
       </>
       }
-      { form === 3 &&
+      { form === "three" &&
       <>
-        <form onSubmit={(e) => { e.preventDefault(); setForm(4); }} className="mt-4 flex flex-col text-left w-full">
+        <form onSubmit={(e) => { e.preventDefault(); setForm("four"); }} className="mt-4 flex flex-col text-left w-full">
           <label className="mb-1">ZIP Code</label>
           <input name="zip"
             required
@@ -155,22 +179,24 @@ function RegisterForm({ register }){
           <input name="radius"
             value={formData.radius}
             onChange={handleChange}
+            pattern="[0-9]"
             type="text"
             className="mb-3 rounded-lg px-2 py-1"
             />
+          {error && <p className="mb-3 text-red-400 font-bold text-center">{error}</p>}
           <div className="flex justify-end">
           <button className="bg-emerald-300 w-fit px-3 py-1 text-sm rounded-lg hover:scale-105">Continue <i className="bi bi-arrow-right"></i></button>
           </div>
         </form>
         <div className="mt-4 flex gap-2">
-        <i onClick={(e) => { e.preventDefault(); setForm(1); }} className="hover:cursor-pointer bi bi-circle-fill text-white text-[8px]"></i>
-        <i onClick={(e) => { e.preventDefault(); setForm(2); }} className="hover:cursor-pointer bi bi-circle-fill text-white text-[8px]"></i>
+        <i onClick={(e) => { e.preventDefault(); setForm("one"); setError(null); }} className="hover:cursor-pointer bi bi-circle-fill text-white text-[8px]"></i>
+        <i onClick={(e) => { e.preventDefault(); setForm("two"); setError(null); }} className="hover:cursor-pointer bi bi-circle-fill text-white text-[8px]"></i>
         <i className="bi bi-circle-fill text-emerald-300 text-[8px]"></i>
         <i className="bi bi-circle-fill text-white text-[8px]"></i>
         </div>
       </>
       }
-      { form === 4 &&
+      { form === "four" &&
       <>
         <form className="mt-4 flex flex-col text-left w-full">
           <label className="mb-1">Profile Photo</label>
@@ -179,7 +205,7 @@ function RegisterForm({ register }){
               type="file"
               className="mb-2 rounded-lg px-2 py-1"
               />
-          <label className="mb-1">Bio </label>
+          <label className="mb-1">Bio</label>
           <input name="bio"
               value={formData.bio}
               onChange={handleChange}
@@ -192,9 +218,9 @@ function RegisterForm({ register }){
           </div>
         </form>
         <div className="mt-4 flex gap-2">
-        <i onClick={(e) => { e.preventDefault(); setForm(1); }} className="hover:cursor-pointer bi bi-circle-fill text-white text-[8px]"></i>
-        <i onClick={(e) => { e.preventDefault(); setForm(2); }} className="hover:cursor-pointer bi bi-circle-fill text-white text-[8px]"></i>
-        <i onClick={(e) => { e.preventDefault(); setForm(3); }} className="hover:cursor-pointer bi bi-circle-fill text-white text-[8px]"></i>
+        <i onClick={(e) => { e.preventDefault(); setForm("one"); setError(null); }} className="hover:cursor-pointer bi bi-circle-fill text-white text-[8px]"></i>
+        <i onClick={(e) => { e.preventDefault(); setForm("two"); setError(null); }} className="hover:cursor-pointer bi bi-circle-fill text-white text-[8px]"></i>
+        <i onClick={(e) => { e.preventDefault(); setForm("three"); setError(null); }} className="hover:cursor-pointer bi bi-circle-fill text-white text-[8px]"></i>
         <i className="bi bi-circle-fill text-emerald-300 text-[8px]"></i>
         </div>
       </>
