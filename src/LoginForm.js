5b1fe70c7ebde4914b284ae080656f51db3b14db
Elise Ngo <elisetvy@gmail.com>
2023-11-14 18:38:08 -0800

    Update login form colors

--- src/LoginForm.js
+++ src/LoginForm.js
@@ -32,40 +32,38 @@ function LoginForm({ login }) {
   }
 
   return (
-    <div className="text-center pt-10">
-      <form onSubmit={handleSubmit} className="bg-emerald-100 w-fit ml-auto mr-auto px-4 py-4 rounded-lg">
-        <div>
-          <label className="mb-1">Username</label>
-          <div>
-            <input name="username"
-              required
-              value={loginData.username}
-              onChange={handleChange}
-              type="text"
-              maxLength={20}
-              className="bg-emerald-300 mb-2 rounded-lg px-2 py-1"
-            />
+    <div className="absolute top-0 left-0 w-screen h-screen flex justify-center items-center">
+      <div className="background-white w-1/3 flex flex-col justify-center items-center text-center px-14 py-10 rounded-lg">
+        <form onSubmit={handleSubmit} className="flex flex-col text-left w-full">
+          <p className="text-xl font-bold font-fuschia text-center">Welcome back!</p>
+            <div className="mt-4">
+            <label className="mb-1 font-purple font-bold">Username</label>
+              <input name="username"
+                required
+                value={loginData.username}
+                onChange={handleChange}
+                type="text"
+                maxLength={20}
+                className="mb-2 rounded-lg px-2 py-1 font-fuschia w-full"
+              />
+            <label className="mb-1 font-purple font-bold">Password</label>
+              <input name="password"
+                required
+                value={loginData.password}
+                onChange={handleChange}
+                type="password"
+                minLength={5}
+                maxLength={20}
+                className="rounded-lg px-2 py-1 font-fuschia w-full"
+              />
           </div>
-        </div>
-        <div>
-          <label className="mb-1">Password</label>
-          <div>
-            <input name="password"
-              required
-              value={loginData.password}
-              onChange={handleChange}
-              type="password"
-              minLength={5}
-              maxLength={20}
-              className="bg-emerald-300 rounded-lg px-2 py-1"
-            />
+          {error && <p className="mt-4 text-red-400 font-bold">{error}</p>}
+          <div className="flex justify-center">
+            <button className="background-fuschia font-white w-fit mt-4 px-3 py-1 rounded-lg hover:scale-105 text-sm">Log In</button>
           </div>
-        </div>
-        {error && <p className="mt-4 text-red-400 font-bold">{error}</p>}
-        <button className="bg-emerald-300 mt-4 px-3 py-1 rounded-lg">Login</button>
-      </form>
+        </form>
+      </div>
     </div>
-
   );
 }
 
