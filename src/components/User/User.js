bfb0fa2ea00850f901cbaca1e45c2279cb12bfb3
Elise Ngo <elisetvy@gmail.com>
2024-07-30 22:39:36 -0700

    Style card w/ grid

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -6,12 +6,10 @@ import "./User.css";
 function User({ user, currUser }) {
 
   return (
-    <Link to={`/users/${user.username}`} className="User min-w-[250px] w-1/5 h-96">
-      <div className="background-white h-full rounded-lg overflow-hidden">
-        <div className="relative h-5/6">
-          <img className="w-full h-full object-cover" src={user.photo} alt={user.username} />
-          <div className="w-full text-left absolute bottom-0 font-white px-2 py-2">{user.name[0].toUpperCase() + user.name.slice(1)}, {user.age}</div>
-        </div>
+    <Link to={`/users/${user.username}`} className="User">
+      <div>
+          <img src={user.photo} alt={user.username} />
+          <div>{user.name[0].toUpperCase() + user.name.slice(1)}, {user.age}</div>
       </div>
     </Link>
   )
