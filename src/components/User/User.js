06c73f9900c187843d2e664139a700c53864642c
Elise Ngo <elisetvy@gmail.com>
2024-07-30 21:55:53 -0700

    Create user folder + css file

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -1,10 +1,12 @@
 import { Link } from "react-router-dom";
 
+import "./User.css";
+
 /** User card. */
 function User({ user, currUser }) {
 
   return (
-    <Link to={`/users/${user.username}`} className="min-w-[250px] w-1/5 h-96">
+    <Link to={`/users/${user.username}`} className="User min-w-[250px] w-1/5 h-96">
       <div className="background-white h-full rounded-lg overflow-hidden">
         <div className="relative h-5/6">
           <img className="w-full h-full object-cover" src={user.photo} alt={user.username} />
