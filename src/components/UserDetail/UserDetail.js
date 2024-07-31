d98642bfb773946d8ad9650976887dd1e5ec8987
Elise Ngo <elisetvy@gmail.com>
2024-07-31 10:37:50 -0700

    Style edit button

--- src/components/UserDetail/UserDetail.js
+++ src/components/UserDetail/UserDetail.js
@@ -63,9 +63,7 @@ function UserDetail({ currUser }) {
           </div>
         </div>
         {user.username === currUser.username &&
-          <div className="text-sm w-fit ml-auto mr-auto mt-3 background-purple font-white px-3 py-1 rounded-lg hover:scale-105 hover:cursor-pointer">
-            <Link className="" to={`/users/${currUser.username}/edit`}>Edit Your Profile</Link>
-          </div>
+          <Link to={`/users/${currUser.username}/edit`}><div className="Edit-button">Edit Your Profile</div></Link>
         }
         <div className="Back">
           <button onClick={() => navigate(-1)}><i className="bi bi-arrow-return-left"></i></button>
