75e181435d116164fec3e987a6992dbccd119f2b
Elise Ngo <elisetvy@gmail.com>
2024-08-01 21:23:21 -0700

    Add navbar

--- src/components/Register/RegisterForm.js
+++ src/components/Register/RegisterForm.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { useNavigate, Link } from "react-router-dom";
 
+import Nav from "../Nav/Nav";
 import "./RegisterForm.css";
 import calculateAge from "../../utils";
 import API from "../../api";
@@ -88,148 +89,151 @@ function RegisterForm({ register }) {
   }
 
   return (
-    <div className="RegisterForm">
-      <div>
+    <div>
+      <Nav className="Nav" />
+      <div className="RegisterForm">
         <div>
-          {form === "one" &&
-            <>
-              <p className="tagline">Be the author of your own love story.</p>
-              <p className="tagline-sub">Choose a username and password to begin.</p>
-              <form onSubmit={checkOne}>
-                <label>Username</label>
-                <input name="username"
-                  required
-                  value={formData.username}
-                  onChange={handleChange}
-                  type="text"
-                  maxLength={20}
-                />
-                <label>Password</label>
-                <input name="password"
-                  required
-                  value={formData.password}
-                  onChange={handleChange}
-                  type="password"
-                  autoComplete="off"
-                  minLength={5}
-                  maxLength={100}
-                />
-                {error && <p className="error">{error}</p>}
-                <div className="Buttons">
-                  <Link className="GoToLogin" to="/login">I already have an account</Link>
-                  <button className="continue">Continue <i className="bi bi-arrow-right"></i></button>
+          <div>
+            {form === "one" &&
+              <>
+                <p className="tagline">Be the author of your own love story.</p>
+                <p className="tagline-sub">Choose a username and password to begin.</p>
+                <form onSubmit={checkOne}>
+                  <label>Username</label>
+                  <input name="username"
+                    required
+                    value={formData.username}
+                    onChange={handleChange}
+                    type="text"
+                    maxLength={20}
+                  />
+                  <label>Password</label>
+                  <input name="password"
+                    required
+                    value={formData.password}
+                    onChange={handleChange}
+                    type="password"
+                    autoComplete="off"
+                    minLength={5}
+                    maxLength={100}
+                  />
+                  {error && <p className="error">{error}</p>}
+                  <div className="Buttons">
+                    <Link className="GoToLogin" to="/login">I already have an account</Link>
+                    <button className="continue">Continue <i className="bi bi-arrow-right"></i></button>
+                  </div>
+                </form>
+                <div className="pagination">
+                  <i className="bi bi-circle-fill active"></i>
+                  <i className="bi bi-circle-fill"></i>
+                  <i className="bi bi-circle-fill"></i>
+                  <i className="bi bi-circle-fill"></i>
                 </div>
-              </form>
-              <div className="pagination">
-                <i className="bi bi-circle-fill active"></i>
-                <i className="bi bi-circle-fill"></i>
-                <i className="bi bi-circle-fill"></i>
-                <i className="bi bi-circle-fill"></i>
-              </div>
-            </>
-          }
-          {form === "two" &&
-            <>
-              <form onSubmit={checkTwo}>
-                <label>Name</label>
-                <input name="name"
-                  required
-                  value={formData.name}
-                  onChange={handleChange}
-                  type="text"
-                  maxLength={20}
-                />
-                <label>Email Address</label>
-                <input name="email"
-                  required
-                  value={formData.email}
-                  onChange={handleChange}
-                  type="email"
-                  maxLength={50}
-                />
-                <label>Date of Birth</label>
-                <input name="dob"
-                  required
-                  value={formData.dob}
-                  onChange={handleChange}
-                  type="date"
-                />
-                {error && <p className="error">{error}</p>}
-                <div className="Buttons">
-                  <button className="GoToLogin" onClick={(e) => { e.preventDefault(); setForm("one"); }}>Back</button>
-                  <button className="continue">Continue <i className="bi bi-arrow-right"></i></button>
+              </>
+            }
+            {form === "two" &&
+              <>
+                <form onSubmit={checkTwo}>
+                  <label>Name</label>
+                  <input name="name"
+                    required
+                    value={formData.name}
+                    onChange={handleChange}
+                    type="text"
+                    maxLength={20}
+                  />
+                  <label>Email Address</label>
+                  <input name="email"
+                    required
+                    value={formData.email}
+                    onChange={handleChange}
+                    type="email"
+                    maxLength={50}
+                  />
+                  <label>Date of Birth</label>
+                  <input name="dob"
+                    required
+                    value={formData.dob}
+                    onChange={handleChange}
+                    type="date"
+                  />
+                  {error && <p className="error">{error}</p>}
+                  <div className="Buttons">
+                    <button className="GoToLogin" onClick={(e) => { e.preventDefault(); setForm("one"); }}>Back</button>
+                    <button className="continue">Continue <i className="bi bi-arrow-right"></i></button>
+                  </div>
+                </form>
+                <div className="pagination">
+                  <i className="bi bi-circle-fill"></i>
+                  <i className="bi bi-circle-fill active"></i>
+                  <i className="bi bi-circle-fill"></i>
+                  <i className="bi bi-circle-fill"></i>
                 </div>
-              </form>
-              <div className="pagination">
-                <i className="bi bi-circle-fill"></i>
-                <i className="bi bi-circle-fill active"></i>
-                <i className="bi bi-circle-fill"></i>
-                <i className="bi bi-circle-fill"></i>
-              </div>
-            </>
-          }
-          {form === "three" &&
-            <>
-              <form onSubmit={(e) => { e.preventDefault(); setForm("four"); }}>
-                <label>ZIP Code</label>
-                <input name="zip"
-                  required
-                  value={formData.zip}
-                  onChange={handleChange}
-                  pattern="[0-9]{5}"
-                  minLength={5}
-                  maxLength={5}
-                />
-                <label>Match Radius</label>
-                <input name="radius"
-                  value={formData.radius}
-                  onChange={handleChange}
-                  pattern="[0-9]{0,}" // input can be empty
-                  type="text"
-                />
-                {error && <p className="error">{error}</p>}
-                <div className="Buttons">
-                <button className="GoToLogin" onClick={(e) => { e.preventDefault(); setForm("two"); }}>Back</button>
-                  <button className="continue">Continue <i className="bi bi-arrow-right"></i></button>
+              </>
+            }
+            {form === "three" &&
+              <>
+                <form onSubmit={(e) => { e.preventDefault(); setForm("four"); }}>
+                  <label>ZIP Code</label>
+                  <input name="zip"
+                    required
+                    value={formData.zip}
+                    onChange={handleChange}
+                    pattern="[0-9]{5}"
+                    minLength={5}
+                    maxLength={5}
+                  />
+                  <label>Match Radius</label>
+                  <input name="radius"
+                    value={formData.radius}
+                    onChange={handleChange}
+                    pattern="[0-9]{0,}" // input can be empty
+                    type="text"
+                  />
+                  {error && <p className="error">{error}</p>}
+                  <div className="Buttons">
+                    <button className="GoToLogin" onClick={(e) => { e.preventDefault(); setForm("two"); }}>Back</button>
+                    <button className="continue">Continue <i className="bi bi-arrow-right"></i></button>
+                  </div>
+                </form>
+                <div className="pagination">
+                  <i className="bi bi-circle-fill"></i>
+                  <i className="bi bi-circle-fill"></i>
+                  <i className="bi bi-circle-fill active"></i>
+                  <i className="bi bi-circle-fill"></i>
                 </div>
-              </form>
-              <div className="pagination">
-                <i className="bi bi-circle-fill"></i>
-                <i className="bi bi-circle-fill"></i>
-                <i className="bi bi-circle-fill active"></i>
-                <i className="bi bi-circle-fill"></i>
-              </div>
-            </>
-          }
-          {form === "four" &&
-            <>
-              <form>
-                <label>Profile Photo</label>
-                <input name="photo"
-                  onChange={handleChange}
-                  type="file"
-                  accept="image/*"
-                />
-                <label>Bio</label>
-                <input name="bio"
-                  value={formData.bio}
-                  onChange={handleChange}
-                  type="text"
-                />
-                {error && <p className="error">{error}</p>}
-                <div className="Buttons">
-                <button className="GoToLogin" onClick={(e) => { e.preventDefault(); setForm("three"); }}>Back</button>
-                  <button onClick={handleSubmit} className="continue">Submit</button>
+              </>
+            }
+            {form === "four" &&
+              <>
+                <form>
+                  <label>Profile Photo</label>
+                  <input name="photo"
+                    onChange={handleChange}
+                    type="file"
+                    accept="image/*"
+                  />
+                  <label>Bio</label>
+                  <input name="bio"
+                    value={formData.bio}
+                    onChange={handleChange}
+                    type="text"
+                  />
+                  {error && <p className="error">{error}</p>}
+                  <div className="Buttons">
+                    <button className="GoToLogin" onClick={(e) => { e.preventDefault(); setForm("three"); }}>Back</button>
+                    <button onClick={handleSubmit} className="continue">Submit</button>
+                  </div>
+                </form>
+                <div className="pagination">
+                  <i className="bi bi-circle-fill"></i>
+                  <i className="bi bi-circle-fill"></i>
+                  <i className="bi bi-circle-fill"></i>
+                  <i className="bi bi-circle-fill active"></i>
                 </div>
-              </form>
-              <div className="pagination">
-                <i className="bi bi-circle-fill"></i>
-                <i className="bi bi-circle-fill"></i>
-                <i className="bi bi-circle-fill"></i>
-                <i className="bi bi-circle-fill active"></i>
-              </div>
-            </>
-          }
+              </>
+            }
+          </div>
         </div>
       </div>
     </div>
